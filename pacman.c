1cbb2e7e00ea58fceaa682d88df74dab36cce900
houstain <33086960+houstain@users.noreply.github.com>
2017-10-31 23:48:22 +0200

    Add files via upload

--- pacman.c
+++ pacman.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void Table(int *x, int *y, int N, int M, char *direction){
+void Table(int *x, int *y, int N, int M, char *direction, int xobs[], int yobs[]){
     int i, j;
     for (i = 0; i <= N + 1; i++){
         for (j = 0; j <= M + 1; j++){
             if ((i == 0) || (j == 0) || (i == N + 1) || (j == M + 1)) printf("*");
             else if ((i == *y) && (j == *x)) printf("%c", *direction);
+			else if ((i == yobs[1]) && (j == xobs[1])) printf("x");
+			else if ((i == yobs[2]) && (j == xobs[2])) printf("x");
+			else if ((i == yobs[3]) && (j == xobs[3])) printf("x");
             else printf(" ");
         }
         printf("\n");
@@ -40,28 +44,73 @@ void MoveRight(int *x, int M, char *direction){
     }
 }
 
+void MoveObstacle(int x, int y, int *xobs, int *yobs){
+	int Length, minLength = 35700, i, j, x2, y2;
+
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++) {
+			Length = abs(x - (*xobs + 1 - j)) + abs(y - (*yobs + i - 1));
+			if (Length < minLength){
+				 minLength = Length;
+				 x2 = (*xobs + 1 - j);
+				 y2 = (*yobs + i - 1);
+			}
+		}
+	} 	
+
+	*xobs = x2;
+	*yobs = y2;
+}
+
 int main()
 {
-    int N, M, x, y, P;
-    char action = 'a', direction = 'v';
+	int N, M, x, y, P, i, xobs[4] = {0}, yobs[4] = {0}, flag = 0, turn = 1;
+    char action = 'a', direction = 'v', type[4];
     
     scanf("%d %d %d %d %d", &N, &M, &x, &y, &P);
-   
+
+	if (P > 0){
+		 for (i = 1; i <= P; i++) 
+			scanf ("%d %d %c", &yobs[i], &xobs[i], &type[i]);
+	}
+	
     while(action){
-        if ((action >= 'a') && (action <= 'z'))
-			Table(&x, &y, N, M, &direction); 
-        scanf("%c", &action);
+        if ((action >= 'a') && (action <= 'z')){
+			//turn++;
+			Table(&x, &y, N, M, &direction, xobs, yobs); 
+		}
+
+		scanf("%c", &action);
+		
         if (action == 'q') break;
-        if (action == 'w') MoveUp(&y, &direction);
+        else if (action == 'w') MoveUp(&y, &direction);
         else if (action == 's') MoveDown(&y, N, &direction);
         else if (action == 'a') MoveLeft(&x, &direction);
         else if (action == 'd') MoveRight(&x, M, &direction);
+
+		//if (turn % 2 == 1){
+			for (i = 1; i <= P; i++){
+				if (type[i] == 'm') 
+					MoveObstacle(x, y, &xobs[i], &yobs[i]); 
+			}
+		//}
+
         if ((x == M) && (y == N)) {
-			Table(&x, &y, N, M, &direction);
+			Table(&x, &y, N, M, &direction, xobs, yobs);
             printf("GAME COMPLETED\n");
             break;
-        } 
-    } 
-   
+        }
+ 
+		for (i = 1; i <= P; i++){
+			if ((x == xobs[i]) && (y == yobs[i])){
+				printf("GAME OVER\n");
+				flag = 1;
+				break;
+			}
+	    }
+
+		if (flag == 1) break;
+	}  
+
 	return 0;
 }
